eb129aace07daebbba929ad06b358d38b5ca5724
fbckdgns3@naver.com <fbckdgns3@naver.com>
2024-03-17 21:31:29 +0900

    refactor: 이름 미 입력시 추가 구현

--- crafeman/src/crafeman/app.py
+++ crafeman/src/crafeman/app.py
@@ -1,30 +1,35 @@
-"""
-CR Developer
-"""
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
 
-class Crafeman(toga.App):
+
+def greeting(name):
+    if name:
+        return f"Hello, {name}"
+    else:
+        return "Hello, stranger"
+
+
+
+class HelloWorld(toga.App):
     def startup(self):
-        
-        main_box = toga.Box(style = Pack(direction = COLUMN))
+        main_box = toga.Box(style=Pack(direction=COLUMN))
 
         name_label = toga.Label(
-            'Your Name: ',
-            style = Pack(padding = (0, 5))
+            "Your name: ",
+            style=Pack(padding=(0, 5))
         )
-        self.name_input = toga.TextInput(style = Pack(flex = 1))
+        self.name_input = toga.TextInput(style=Pack(flex=1))
 
-        name_box = toga.Box(style = Pack(direction = ROW, padding = 5))
+        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
         name_box.add(name_label)
         name_box.add(self.name_input)
 
         button = toga.Button(
-            'Hello! Python Android',
-            on_press = self.say_hello,
-            style = Pack(padding = 5)
+            "Say Hello!",
+            on_press=self.say_hello,
+            style=Pack(padding=5)
         )
 
         main_box.add(name_box)
@@ -34,13 +39,15 @@ class Crafeman(toga.App):
         self.main_window.content = main_box
         self.main_window.show()
 
+
     def say_hello(self, widget):
         self.main_window.info_dialog(
-            f"hello, {self.name_input.value}",
-            "Hi there"
+            greeting(self.name_input.value),
+            "Hi there!"
         )
-        
+
+
 
 def main():
-    return Crafeman()
+    return HelloWorld()
 
